6930976dd060ccfeb88c99628a911ebe1cb22739
constantini21 <gustavoconstantinimattos.21@gmail.com>
2020-08-16 12:22:10 -0300

    remove back-end

--- back-end/src/config/jwt-strategy.config.ts
+++
@@ -1,7 +0,0 @@
-import { ExtractJwt, StrategyOptions } from 'passport-jwt';
-
-export const jwtStrategyConfig: StrategyOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  ignoreExpiration: false,
-  secretOrKey: process.env.JWT_SECRET,
-};
